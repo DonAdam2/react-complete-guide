3d5156adccb61eeb680f0d95ad6819f5b8f08087
Adam Morsi <adam.morsi@yahoo.com>
2018-06-07 12:16:05 +0300

    end of section 3

--- src/App.js
+++ src/App.js
@@ -11,19 +11,46 @@ class App extends Component {
         ]
     };
 
-    swithNameHandler = () => {
-      console.log('Was clicked');
+    switchNameHandler = (newName) => {
+        this.setState({
+            persons: [
+                {name: newName, age: 28},
+                {name: 'Manu', age: 29},
+                {name: 'stephanie', age: 27}
+            ]
+        });
+    };
+
+    nameChangedHandler = (event) => {
+        this.setState({
+            persons: [
+                {name: 'Max', age: 28},
+                {name: event.target.value, age: 29},
+                {name: 'stephanie', age: 27}
+            ]
+        });
     };
 
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 <h1>Hi, i'm a React App</h1>
                 <p>This is really working!</p>
-                <button onClick={this.swithNameHandler}>Switch Name</button>
+                <button style={style} onClick={this.switchNameHandler}>Switch Name</button>
                 <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
-                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}
+                        changed={this.nameChangedHandler} click={this.switchNameHandler.bind(this, 'Adam')}>My Hobbies:
+                    Racing</Person>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}
+                        click={() => this.switchNameHandler('Bassant')}/>
             </div>
         );
     }
